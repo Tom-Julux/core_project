3854fa511f6b50e2a0a83cc490d28ad02f8b8dce
Tom-Julux <42802270+Tom-Julux@users.noreply.github.com>
2025-11-26 19:22:08 +0100

    Update shape based interpolation

--- napari-shape-based-interpolation/src/napari_shape_based_interpolation/_widget_shape_based_interpolation.py
+++ napari-shape-based-interpolation/src/napari_shape_based_interpolation/_widget_shape_based_interpolation.py
@@ -30,6 +30,7 @@ from napari_toolkit.widgets import (
     setup_lineedit,
     setup_fileselect,
     setup_savefileselect,
+    setup_plaintextedit,
     setup_labeledslider,
     setup_pushbutton,
     setup_hswitch,
@@ -47,7 +48,10 @@ from qtpy.QtWidgets import (
     QFileDialog,
     QSizePolicy,
     QVBoxLayout,
-    QWidget,
+    QWidget
+)
+from qtpy.QtGui import (
+    QTextOption
 )
 import traceback
 
@@ -75,14 +79,39 @@ def interpolate_labels(label_a, label_b, num_steps):
     labels_interp = labels_interp.transpose(1,2,0)
     labels_interp = (labels_interp <= 0).astype(np.uint8)
     return labels_interp
-    
+
+class LogFileWidget(QWidget):
+    def __init__(self, parent):
+        super().__init__(parent=parent)
+
+        main_layout = QVBoxLayout(self)
+
+        _container, _layout = setup_vcollapsiblegroupbox(
+            main_layout, "Log:", collapsed=False)
+        _layout.setContentsMargins(0,0,0,0)
+
+        _scroll_widget, _layout = setup_vscrollarea(_layout)
+
+        self.text_log = setup_plaintextedit(
+            _layout, "", "", function=lambda: print("QPlainTextEdit"), readonly=True
+        )
+        self.text_log.setWordWrapMode(QTextOption.NoWrap)
+
+    def append_log(self, message: str):
+        self.text_log.appendPlainText(message)
+
+    def set_log(self, message: str):
+        self.text_log.setPlainText(message)
+
+    def clear_log(self):
+        self.text_log.setPlainText("")
+
 class ShapeBasedInterpolationWidget(QWidget):
     def __init__(self, viewer: Viewer):
         super().__init__()
         self._viewer = viewer
 
-        self.active_layer = None
-        
+        self.current_layer = None
         self.current_index = None
         self.manual_frames = None
         self.silence_events = False
@@ -90,25 +119,50 @@ class ShapeBasedInterpolationWidget(QWidget):
         main_layout = QVBoxLayout(self)
 
         _scroll_widget, _scroll_layout = setup_vscrollarea(main_layout)
+        _scroll_layout.setContentsMargins(0,0,0,0)
+        _scroll_widget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
+        _ = setup_label(
+            _scroll_layout, "Select label layer:")
         # layer select for image layer
         self.layerselect_a = setup_layerselect(
             _scroll_layout, self._viewer, Labels, function=lambda: self.on_layer_change()
         )
-
+        
         self.run_button = setup_iconbutton(
             _scroll_layout,
-            "Start",
+            "Interpolate",
             "right_arrow",
             self._viewer.theme,
             function=lambda: self.run_shape_based_interpolation()
         )
+     
+        self.clear_button = setup_iconbutton(
+            _scroll_layout,
+            "Revert Interpolations",
+            "erase",
+            self._viewer.theme,
+            function=lambda: self.clear_interpolated_frames()
+        )
 
+        _container, _layout = setup_vcollapsiblegroupbox(
+            _scroll_layout, "Settings:", collapsed=False)
+
+        self.overwrite_existing_mm_ckbx = setup_checkbox(
+            _layout,
+            "Overwrite other labels (irreversible)",
+            False,
+            tooltips="If unchecked, interpolated frames will only be added to empty regions. If checked, interpolations will overwrite existing labels."
+        )
+
+        #self.autorun_checkbox = setup_checkbox(
+        #    _scroll_layout,
+        #    "Auto Run",
+        #    False)
+        
         self.setup_connections()
 
         self.current_index = None
-
-
     def on_layer_change(self):
         pass
         """
@@ -116,6 +170,12 @@ class ShapeBasedInterpolationWidget(QWidget):
         image layer to preview and prompt layers so they follow image
         transformations.
         """
+        if self.current_layer is not None:
+            self.current_layer.events.set_data.disconnect(self._on_set_data)
+            self.current_layer.events.labels_update.disconnect(self._on_labels_update)
+            self.current_layer.events.selected_label.disconnect(self.on_selected_label_update)
+            self.current_layer = None
+
         label_layer, img_layer_idx = get_value(self.layerselect_a)
 
         if label_layer not in self._viewer.layers:
@@ -123,34 +183,40 @@ class ShapeBasedInterpolationWidget(QWidget):
         
         self.setup_connections()
 
-    def clear(self):
-        pass
-
+    @thread_worker(start_thread=True)
     def run_shape_based_interpolation(self):
-        label_layer, img_layer_idx = get_value(self.layerselect_a)
-        
-        label_data = np.transpose(
-            self._viewer.layers[label_layer].data, self._viewer.dims.order)
+
+        if self.current_layer is None:
+            show_warning("Please select a valid label layer.")
+            return
+        label_layer = self.current_layer
+
+        label_data = np.transpose(label_layer.data, self._viewer.dims.order)
         N = len(label_data.shape)
+
         if N != 3:
             show_warning("Shape base interpolation only works on 3D data.")
             return
+        
         current_index = self._viewer.dims.current_step[self._viewer.dims.order[0]]
 
-        if self.manual_frames is not None:
+        if label_layer.selected_label == 0:
+            show_warning("Please select a valid label (non-zero).")
+            return
+
+        new_labels = label_data.copy()
+
+        if self.manual_frames is not None and len(self.manual_frames) >= 2:
             non_empty_labels = self.manual_frames
         else:
-            non_empty_labels = np.where(label_data.sum(axis=(1,2)) != 0)[0]
+            non_empty_labels = np.where(new_labels.sum(axis=(1,2)) == label_layer.selected_label)[0]
             self.manual_frames = non_empty_labels.tolist()
-        print(f"Non empty labels found at frames: {non_empty_labels}")
 
         if len(non_empty_labels) < 2:
             show_warning("At least two frames with labels are required for shape based interpolation.")
             return
-        #label_data_sbi = label_data.transpose(1,2,0)
 
-        print(non_empty_labels)
-        new_labels = label_data.copy()
+        #print(non_empty_labels)
         # for each pair of non empty labels, interpolate
         for i in range(len(non_empty_labels) - 1):
             frame_a = non_empty_labels[i]
@@ -158,32 +224,77 @@ class ShapeBasedInterpolationWidget(QWidget):
             num_steps = frame_b - frame_a + 1
             if num_steps <= 2:
                 continue
-            print(f"Interpolating between frames {frame_a} and {frame_b} with {num_steps} steps.")
-            labels_a = label_data[frame_a]
-            labels_b = label_data[frame_b]
+            #print(f"Interpolating between frames {frame_a} and {frame_b} with {num_steps} steps.")
+            labels_a = (label_data[frame_a] == label_layer.selected_label).astype(np.uint8)
+            labels_b = (label_data[frame_b] == label_layer.selected_label).astype(np.uint8)
+            
             labels_interp = interpolate_labels(labels_a, labels_b, num_steps)
+            
+            labels_interp = labels_interp[1:-1] * label_layer.selected_label
 
-            new_labels[frame_a+1:frame_b] = labels_interp[1:-1]
+            # assign interpolated labels to new_labels if not overwriting existing
+            overlayed = np.logical_or(new_labels[frame_a+1:frame_b] == 0, new_labels[frame_a+1:frame_b] == label_layer.selected_label)
+            
+            if get_value(self.overwrite_existing_mm_ckbx):
+                overlayed = np.logical_or(overlayed, labels_interp == label_layer.selected_label)
+            
+            #labels_to_assign[overlayed] = new_labels[frame_a+1:frame_b][overlayed]
+            new_labels[frame_a+1:frame_b][overlayed] = labels_interp[overlayed]
 
         non_empty_labels_new = np.where(new_labels.sum(axis=(1,2)) != 0) 
-        print(f"Non empty labels found at frames: {non_empty_labels_new}")
-
-        print("Shape based interpolation done.")
         self.silence_events = True
         np.copyto(label_data, new_labels)
         self.silence_events = False
-        self._viewer.layers[label_layer].refresh()
+        label_layer.refresh()
+        show_info("Shape based interpolation completed.")
+
+    @thread_worker(start_thread=True)
+    def clear_interpolated_frames(self):
+        if self.current_layer is None:
+            show_warning("Please select a valid label layer.")
+            return
+        label_layer = self.current_layer
+
+        label_data = np.transpose(label_layer.data, self._viewer.dims.order)
+        N = len(label_data.shape)
+        if N != 3:
+            show_warning("Shape base interpolation only works on 3D data.")
+            return
+
+        if self.manual_frames is None or len(self.manual_frames) <= 2:
+            return
+
+        new_labels = label_data.copy()
+        # for each pair of non empty labels, clear interpolated frames
+        for i in range(len(non_empty_labels) - 1):
+            frame_a = non_empty_labels[i]
+            frame_b = non_empty_labels[i+1]
+            if frame_b - frame_a <= 1:
+                continue
+
+            # assign interpolated labels to new_labels if not overwriting existing
+            overlayed = new_labels[frame_a+1:frame_b] == label_layer.selected_label
+            
+            #labels_to_assign[overlayed] = new_labels[frame_a+1:frame_b][overlayed]
+            new_labels[frame_a+1:frame_b][overlayed] = 0
+
+        self.silence_events = True
+        np.copyto(label_data, new_labels)
+        self.silence_events = False
+        label_layer.refresh()
+        show_info("Cleared interpolated frames.")
+
 
     def setup_connections(self):
         label_layer, img_layer_idx = get_value(self.layerselect_a)
         if label_layer is None or img_layer_idx == -1 or label_layer not in self._viewer.layers:
             return
-        print(f"Label layer selected: {label_layer}, idx: {img_layer_idx}")
-
         label_layer = self._viewer.layers[label_layer]
+        self.current_layer = label_layer
 
-        label_layer.events.set_data.connect(self._on_set_data)
-        label_layer.events.labels_update.connect(self._on_labels_update)
+        self.current_layer.events.set_data.connect(self._on_set_data)
+        self.current_layer.events.labels_update.connect(self._on_labels_update)
+        self.current_layer.events.selected_label.connect(self.on_selected_label_update)
     
     def _on_set_data(self, event):
         """sync data modification from additional viewers"""
@@ -193,7 +304,6 @@ class ShapeBasedInterpolationWidget(QWidget):
 
         if self.silence_events:
             return
-
 
     def _on_labels_update(self, event):
         """sync data modification from additional viewers"""
@@ -216,7 +326,17 @@ class ShapeBasedInterpolationWidget(QWidget):
         elif current_index not in self.manual_frames:
             self.manual_frames.append(current_index)
             self.manual_frames.sort()
-            print(f"Added frame {current_index} to manual frames: {self.manual_frames}")        
+
+    def on_selected_label_update(self, event):
+        if self.current_layer is None:
+            show_warning("Please select a valid label layer.")
+            return
+        label_layer = self.current_layer
+        
+        selected_label = label_layer.selected_label
+
+        # reset manual frames
+        self.manual_frames = None
 
     def showEvent(self, event):
         self.setup_connections()
@@ -225,12 +345,11 @@ class ShapeBasedInterpolationWidget(QWidget):
         self.hideEvent(event)
 
     def hideEvent(self, event):
-        label_layer, img_layer_idx = get_value(self.layerselect_a)
-        if label_layer is None or img_layer_idx == -1 or label_layer not in self._viewer.layers:
+        self.manual_frames = None
+
+        if self.current_layer is None:
             return
-        print(f"Label layer selected: {label_layer}, idx: {img_layer_idx}")
-
-        label_layer = self._viewer.layers[label_layer]
-
-        label_layer.events.set_data.disconnect(self._on_set_data)
-        label_layer.events.labels_update.disconnect(self._on_labels_update)
+        self.current_layer.events.set_data.disconnect(self._on_set_data)
+        self.current_layer.events.labels_update.disconnect(self._on_labels_update)
+        self.current_layer.events.selected_label.disconnect(self.on_selected_label_update)
+        self.current_layer = None
