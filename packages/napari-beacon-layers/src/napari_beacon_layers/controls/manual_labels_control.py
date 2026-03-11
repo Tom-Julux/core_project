551a4cdbffcd71c81f2151f6e20f173a30598a03
Tom-Julux <42802270+Tom-Julux@users.noreply.github.com>
2026-03-11 19:28:46 +0100

    push

--- packages/napari-beacon-layers/src/napari_beacon_layers/controls/manual_labels_control.py
+++ packages/napari-beacon-layers/src/napari_beacon_layers/controls/manual_labels_control.py
@@ -4,7 +4,6 @@ import napari
 from packaging.version import Version
 
 import numpy as np
-from scipy.ndimage import binary_fill_holes
 
 from qtpy.QtCore import Qt
 from qtpy.QtWidgets import (
@@ -24,6 +23,9 @@ from napari.utils._dtype import get_dtype_limits
 from napari.utils.translations import trans
 from napari._qt.utils import attr_to_settr, checked_to_bool
 from napari.utils.events.event_utils import connect_setattr
+from napari_toolkit.containers.boxlayout import hstack, vstack
+from napari_toolkit.widgets import setup_pushbutton
+from qtpy.QtWidgets import QHBoxLayout, QVBoxLayout
 
 class QtShapeBasedInterpolationControl(QtWidgetControlsBase):
     """
@@ -119,6 +121,102 @@ class QtAutofillCheckBoxControl(QtWidgetControlsBase):
     def get_widget_controls(self) -> list[tuple[QtWrappedLabel, QWidget]]:
         return [(self.autofill_checkbox_label, self.autofill_checkbox)]
 
+class QtShapeBasedInterpolationCheckBoxControl(QtWidgetControlsBase):
+    """
+    Class that wraps the connection of events/signals between the layer shape_based_interpolation
+    attribute and Qt widgets.
+
+    Parameters
+    ----------
+    parent: qtpy.QtWidgets.QWidget
+        An instance of QWidget that will be used as widgets parent
+    layer : napari_beacon_layers.ManualLabelsLayer
+        An instance of a napari_beacon_layers ManualLabelsLayer layer.
+
+    Attributes
+    ----------
+    shape_based_interpolation_checkbox : qtpy.QtWidgets.QCheckBox
+        Checkbox to control if shape-based interpolation is used.
+    shape_based_interpolation_label : napari._qt.layer_controls.widgets.qt_widget_controls_base.QtWrappedLabel
+        Label for the shape-based interpolation chooser widget.
+    """
+
+    def __init__(self, parent: QWidget, layer: Labels) -> None:
+        super().__init__(parent, layer)
+        # Setup widgets
+       
+        self.shape_based_interpolation_label = QtWrappedLabel('shape-based\ninterpolation:')
+        self.shape_based_interpolation_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        max_frame = max(int(layer.data.shape[0]) - 1, 0)
+        self.start_spinbox = QLargeIntSpinBox()
+        self.start_spinbox.setRange(0, max_frame)
+        self.start_spinbox.setValue(int(layer.shape_interpolation_start))
+        self.start_spinbox.setToolTip("Start frame for shape-based interpolation")
+
+        connect_setattr(
+            self.start_spinbox.valueChanged,
+            layer,
+            "shape_interpolation_start",
+        )
+
+        self.stop_spinbox = QLargeIntSpinBox()
+        self.stop_spinbox.setRange(0, max_frame)
+        self.stop_spinbox.setValue(int(layer.shape_interpolation_stop))
+        self.stop_spinbox.setToolTip("Stop frame for shape-based interpolation")
+
+        connect_setattr(
+            self.stop_spinbox.valueChanged,
+            layer,
+            "shape_interpolation_stop",
+        )
+
+        def _current_frame() -> int:
+            viewer = napari.current_viewer()
+            if viewer is None:
+                return 0
+            axis = viewer.dims.order[0]
+            return int(viewer.dims.current_step[axis])
+
+        self.set_start_current_btn = setup_pushbutton(
+            layout=None,
+            text="Set",
+            function=lambda: self.start_spinbox.setValue(_current_frame()),
+        )
+        self.set_start_current_btn.setFixedHeight(self.start_spinbox.sizeHint().height()-1)
+
+        self.start_controls = QWidget()
+        start_layout = QHBoxLayout()
+        start_layout.setSpacing(0)
+        start_layout.setContentsMargins(0,0,0,0)
+        start_layout.addWidget(self.start_spinbox, stretch=1)
+        start_layout.addWidget(self.set_start_current_btn, stretch=0)
+        self.start_controls.setLayout(start_layout)
+
+        self.set_stop_current_btn = setup_pushbutton(
+            layout=None,
+            text="Set",
+            function=lambda: self.stop_spinbox.setValue(_current_frame()),
+        )
+        self.set_stop_current_btn.setFixedHeight(self.stop_spinbox.sizeHint().height()-1)
+
+        self.stop_controls = QWidget()
+        stop_layout = QHBoxLayout()
+        stop_layout.setSpacing(0)
+        stop_layout.setContentsMargins(0,0,0,0)
+        stop_layout.addWidget(self.stop_spinbox, stretch=1)
+        stop_layout.addWidget(self.set_stop_current_btn, stretch=0)
+        self.stop_controls.setLayout(stop_layout)
+
+        self.apply_btn = setup_pushbutton(layout=None, text="Apply", function=lambda: layer.apply_shape_based_interpolation())
+        
+        self.shape_based_interpolation_start_label = QtWrappedLabel('start:')
+        self.shape_based_interpolation_stop_label = QtWrappedLabel('stop:')
+    
+    def get_widget_controls(self) -> list[tuple[QtWrappedLabel, QWidget]]:
+        return [(self.shape_based_interpolation_label, self.apply_btn),
+        (self.shape_based_interpolation_start_label, self.start_controls),
+        (self.shape_based_interpolation_stop_label, self.stop_controls)]
 
 class CustomQtManualLabelsControls(QtLabelsControls):
     """Custom Qt controls for labels layer used for previewing labels.
@@ -163,11 +261,6 @@ class CustomQtManualLabelsControls(QtLabelsControls):
             button["button"].hide()
             action_manager.unbind_shortcut(button["shortcut"])
 
-        # TODO
-        # shape based interpolation
-        # start button
-        # apply button
-
         # quickly switch to erase mode when holding shift and switch back to paint mode when releasing shift
         @layer.bind_key('Shift')
         def quick_switch_tool(viewer):
@@ -200,28 +293,7 @@ class CustomQtManualLabelsControls(QtLabelsControls):
         self._autofill_checkbox_control = QtAutofillCheckBoxControl(self, layer)
         self._add_widget_controls(self._autofill_checkbox_control)
 
-        def autofill(event):
-            if not layer.autofill:
-                return
-                
-            viewer = napari.current_viewer()
-            # get current view
-            transposed_layer_data = np.transpose(layer.data.copy(), viewer.dims.order)
-            # get current slice in that view
-            current_slice = (transposed_layer_data.shape[0] - 1 - viewer.dims.current_step[viewer.dims.order[0]]) \
-                if layer.scale[viewer.dims.order[0]] < 0 else \
-                viewer.dims.current_step[viewer.dims.order[0]]
-            current_slice_data = transposed_layer_data[current_slice]
+        if len(layer.data.shape) == 3:
+            self._shape_based_interpolation_control = QtShapeBasedInterpolationCheckBoxControl(self, layer)
+            self._add_widget_controls(self._shape_based_interpolation_control)
 
-            # fill holes using 
-            filled_slice = binary_fill_holes(current_slice_data == layer.selected_label)   
-
-            # set the filled slice back to the layer data
-            transposed_layer_data[current_slice] = filled_slice.astype(layer.data.dtype) * layer.selected_label
-            # set the layer data back to the layer         
-            with layer.events.blocker():
-                np.copyto(layer.data, np.transpose(transposed_layer_data, viewer.dims.order))
-                layer.refresh()  # refresh the layer to update the view
-
-        layer.events.paint.connect(autofill)
-        
--- packages/napari-beacon-layers/src/napari_beacon_layers/layers/manual_labels_layer.py
+++ packages/napari-beacon-layers/src/napari_beacon_layers/layers/manual_labels_layer.py
@@ -8,6 +8,29 @@ from napari.utils.events import EmitterGroup, Event
 
 from napari._qt.layer_controls.qt_layer_controls_container import layer_to_controls
 from napari_beacon_layers.controls.manual_labels_control import CustomQtManualLabelsControls
+from napari.utils.notifications import show_info, show_warning, show_error, show_console_notification
+
+import SimpleITK as sitk
+from scipy.ndimage import binary_fill_holes
+from scipy.interpolate import interpn
+
+def interpolate_labels(label_a, label_b, num_steps):
+    sitk_img_a = sitk.GetImageFromArray(label_a)
+    sitk_distance_map_a = sitk.GetArrayFromImage(sitk.ApproximateSignedDistanceMap(sitk_img_a))
+    
+    sitk_img_b = sitk.GetImageFromArray(label_b)
+    sitk_distance_map_b = sitk.GetArrayFromImage(sitk.ApproximateSignedDistanceMap(sitk_img_b))
+    
+    distance_map= np.zeros((2,) + label_a.shape, dtype=np.float32)
+    distance_map[0,:,:] = sitk_distance_map_a
+    distance_map[1,:,:] = sitk_distance_map_b
+    
+    x, y, z = np.meshgrid(np.linspace(0, num_steps-1, num_steps), np.arange(label_a.shape[0]), np.arange(label_a.shape[1]))
+    
+    labels_interp = interpn((np.array([0,num_steps-1]), np.arange(label_a.shape[0]), np.arange(label_a.shape[1])), distance_map, np.array([x,y,z]).T, bounds_error=False, fill_value=0)
+    labels_interp = labels_interp.transpose(1,2,0)
+    labels_interp = (labels_interp <= 0).astype(np.uint8)
+    return labels_interp
 
 
 class ManualLabelsLayer(Labels):
@@ -20,10 +43,17 @@ class ManualLabelsLayer(Labels):
     def __init__(self, data, *args, **kwargs):
         super().__init__(data, *args, **kwargs)
         self.mode = 'paint'  # default mode is paint
-        self._autofill = True  # whether to autofill connected components after painting
+
+        self._autofill = True
         self.events.add(autofill=Event) 
-        self._shape_based_interpolation = True  # whether to autofill connected components after painting
-        self.events.add(shape_based_interpolation=Event) 
+        self.events.paint.connect(lambda event: self.apply_autofill() if self.autofill else None)
+        self.events.autofill.connect(lambda event: self.apply_autofill() if self.autofill else None)
+
+        self._shape_interpolation_start = 0
+        self._shape_interpolation_stop = 0
+        self.events.add(shape_interpolation_start=Event)
+        self.events.add(shape_interpolation_stop=Event)
+
     
     @property
     def autofill(self):
@@ -35,15 +65,93 @@ class ManualLabelsLayer(Labels):
         self._autofill = autofill
         self.events.autofill()
 
-    @property
-    def shape_based_interpolation(self):
-        """bool: whether to use shape-based interpolation."""
-        return self._shape_based_interpolation
+    def apply_autofill(self):
+        viewer = napari.current_viewer()
+        # get current view
+        transposed_layer_data = np.transpose(self.data.copy(), viewer.dims.order)
+        # get current slice in that view
+        current_slice = (transposed_layer_data.shape[0] - 1 - viewer.dims.current_step[viewer.dims.order[0]]) \
+            if self.scale[viewer.dims.order[0]] < 0 else \
+            viewer.dims.current_step[viewer.dims.order[0]]
+        current_slice_data = transposed_layer_data[current_slice]
 
-    @shape_based_interpolation.setter
-    def shape_based_interpolation(self, shape_based_interpolation):
-        self._shape_based_interpolation = shape_based_interpolation
-        self.events.shape_based_interpolation()
+        # fill holes using 
+        filled_slice = binary_fill_holes(current_slice_data == self.selected_label)   
+
+        # set the filled slice back to the layer data
+        transposed_layer_data[current_slice] = filled_slice.astype(self.data.dtype) * self.selected_label
+        # set the layer data back to the layer         
+        with self.events.blocker():
+            np.copyto(self.data, np.transpose(transposed_layer_data, viewer.dims.order))
+            self.refresh()  # refresh the layer to update the view
+    
+    @property
+    def shape_interpolation_start(self):
+        """int: starting frame for shape-based interpolation."""
+        return self._shape_interpolation_start
+    
+    @shape_interpolation_start.setter
+    def shape_interpolation_start(self, shape_interpolation_start):
+        self._shape_interpolation_start = shape_interpolation_start
+        self.events.shape_interpolation_start()
+    
+    @property
+    def shape_interpolation_stop(self):
+        """int: stopping frame for shape-based interpolation."""
+        return self._shape_interpolation_stop
+    
+    @shape_interpolation_stop.setter
+    def shape_interpolation_stop(self, shape_interpolation_stop):
+        self._shape_interpolation_stop = shape_interpolation_stop
+        self.events.shape_interpolation_stop()
+
+    def apply_shape_based_interpolation(self):
+        viewer = napari.current_viewer()
+
+        label_data = self.data
+        N = len(label_data.shape)
+
+        if N != 3:
+            show_warning("Shape base interpolation only works on 3D data.")
+            return
+        
+        if self.selected_label == 0:
+            show_warning("Please select a valid label (non-zero).")
+            return
+
+        new_labels = label_data.copy()
+
+        start_frame = self.shape_interpolation_start
+        stop_frame = self.shape_interpolation_stop
+
+        start_frame = (label_data.shape[0] - 1 - start_frame) \
+            if self.scale[0] < 0 else \
+            start_frame
+        stop_frame = (label_data.shape[0] - 1 - stop_frame) \
+            if self.scale[0] < 0 else \
+            stop_frame
+
+        start_frame, stop_frame = sorted([start_frame, stop_frame])
+
+        num_steps = stop_frame - start_frame + 1
+
+        if num_steps <= 2:
+            return
+
+        print((label_data == self.selected_label).sum(axis=(1, 2)).argmax())
+    
+        start_label = (label_data[start_frame] == self.selected_label).astype(np.uint8)
+        stop_label = (label_data[stop_frame] == self.selected_label).astype(np.uint8)
+        
+        labels_interp = interpolate_labels(start_label, stop_label, num_steps)
+        labels_interp = labels_interp * self.selected_label
+
+        new_labels[start_frame:stop_frame+1] = labels_interp#[overlayed]
+
+        np.copyto(label_data, new_labels)
+        self.refresh()
+        show_info("Shape based interpolation completed.")
+
 
 # register the custom layer controls
 layer_to_controls[ManualLabelsLayer] = CustomQtManualLabelsControls
